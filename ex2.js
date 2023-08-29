0750c053b1fddd030e971b3a1c344fcf56f5f417
Cody Feldhaus <cody.feldhaus@gmail.com>
2023-08-29 16:38:05 -0700

    create and review ex2

---
+++ ex2.js
@@ -0,0 +1,16 @@
+let x = 5;
+for (let i = 0; i < x; i++) {
+  console.log(i);
+}
+
+// /*
+// This
+// is a 
+// multiline
+// comment
+// */
+
+//comments are used to leave human-readable
+//notes in your code, either for your own sake
+//or the sake of someone else who will look
+//at your code in the future.
