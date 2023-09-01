0eb6179464298abe4bbb440fb56054bfd3a39b4f
Cody Feldhaus <cody.feldhaus@gmail.com>
2023-08-31 17:19:03 -0700

    ex15 started

---
+++ onboarding-review/ex15.js
@@ -0,0 +1,13 @@
+// for loops
+
+for (let i = 1; i < 5; i--) {
+  console.log(`Now we're on number ${i}`);
+}
+
+/*
+
+for (ITERATOR, CONTINUATION CONDITION, ITERATOR CHANGE) {
+   //whatever code you want to run each time
+}
+
+ */
